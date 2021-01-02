7f38ee1cff72edefe240d17167d7470195fe9172
peppelinux <giuseppe.demarco@unical.it>
2021-01-02 17:10:54 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -12,17 +12,18 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 #  python3 setup.py sdist
 #  twine upload dist/*
 
-_src_folder = 'src'
-_pkg_name = 'unicms_template_unical'
+SRC_FOLDER = 'src'
+PKG_NAME = 'unicms_template_unical'
 
 setup(
-    name='unicms-template-unical',
-    version='0.2.2',
-    packages=[f"{_pkg_name}"],
-    package_dir={f"{_pkg_name}": f"{_src_folder}/{_pkg_name}"},
+    name=PKG_NAME,
+    version='0.4.2',
 
-    package_data={f"{_pkg_name}": [i.replace(f'{_src_folder}/{_pkg_name}/', '')
-                                   for i in glob(f'{_src_folder}/{_pkg_name}/**',
+    packages=[PKG_NAME],
+    package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
+    
+    package_data={PKG_NAME: [i.replace(f'{SRC_FOLDER}/{PKG_NAME}/', '') 
+                                   for i in glob(f'{SRC_FOLDER}/{PKG_NAME}/**', 
                                                  recursive=True)]
     },
 
