2432847988bd7440a555d992ffba3fc326ebf10f
Francesco Filicetti <francesco.filicetti@unical.it>
2021-06-22 13:03:27 +0200

    fix: head imports

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PKG_NAME = 'unicms_template_unical'
 
 setup(
     name=PKG_NAME,
-    version='0.6.5',
+    version='0.6.6',
 
     packages=[PKG_NAME],
     package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
