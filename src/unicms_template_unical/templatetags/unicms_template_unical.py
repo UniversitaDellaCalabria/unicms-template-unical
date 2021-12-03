6a1649dc204ba67b12fb75eea062b53ca334f546
Francesco Filicetti <francesco.filicetti@unical.it>
2021-12-03 09:34:03 +0100

    fix: templatetags improved

--- src/unicms_template_unical/templatetags/unicms_template_unical.py
+++ src/unicms_template_unical/templatetags/unicms_template_unical.py
@@ -3,28 +3,41 @@ import logging
 from django import template
 
 from cms.contexts.utils import append_slash
+from cms.menus.models import NavigationBarItem
+
 
 logger = logging.getLogger(__name__)
 register = template.Library()
 
 
-def _get_same_level_items(item, path, language, exclude_item=True):
-    webpath = item.webpath
-    if webpath and not webpath.is_alias:
-        if path == webpath.get_full_path():
-            parent = item.parent
-            # if parent:
-            items = parent.get_childs(lang=language, exclude=item) \
-                    if exclude_item \
-                    else parent.get_childs(lang=language)
-            return {'parent': parent.localized(lang=language),
-                    'items': items,
-                    'current': item}
-    for child in item.get_childs():
-        result = _get_same_level_items(child, path, language,
-                                       exclude_item=exclude_item)
-        if result: return result
-    return {}
+# def _get_same_level_items(item, path, language, exclude_item=True):
+def _get_same_level_items(item, language, exclude_item=True):
+    parent = item.parent
+    # if parent:
+    items = parent.get_childs(lang=language, exclude=item) \
+            if exclude_item \
+            else parent.get_childs(lang=language)
+    return {'parent': parent.localized(lang=language),
+            'items': items,
+            'current': item}
+    # return {}
+
+    # webpath = item.webpath
+    # if webpath and not webpath.is_alias:
+        # if path == webpath.get_full_path():
+            # parent = item.parent
+            ## if parent:
+            # items = parent.get_childs(lang=language, exclude=item) \
+                    # if exclude_item \
+                    # else parent.get_childs(lang=language)
+            # return {'parent': parent.localized(lang=language),
+                    # 'items': items,
+                    # 'current': item}
+    # for child in item.get_childs():
+        # result = _get_same_level_items(child, path, language,
+                                       # exclude_item=exclude_item)
+        # if result: return result
+    # return {}
 
 
 @register.simple_tag(takes_context=True)
@@ -35,23 +48,27 @@ def load_same_level_items(context, exclude_item=True):
     request = context['request']
     language = getattr(request, 'LANGUAGE_CODE', '')
 
-    path = context['page'].webpath.get_full_path()
+    # path = context['page'].webpath.get_full_path()
 
-    for item in context['items']:
-        result = _get_same_level_items(item, path, language,
-                                       exclude_item=exclude_item)
-        if result: return result
+    item = NavigationBarItem.objects.filter(menu=context['items'][0].menu,
+                                            is_active=True,
+                                            webpath=context['page'].webpath,
+                                            webpath__alias__isnull=True,
+                                            webpath__is_active=True).first()
+    if item:
+        result = _get_same_level_items(item, language, exclude_item=exclude_item)
+        return result
 
 
-def _get_current_item(item, path, language):
-    webpath = item.webpath
-    if webpath and not webpath.is_alias:
-        if path == webpath.get_full_path():
-            return item
-    for child in item.get_childs():
-        result = _get_current_item(child, path, language)
-        if result: return result
-    return None # pragma: no cover
+# def _get_current_item(item, path, language):
+    # webpath = item.webpath
+    # if webpath and not webpath.is_alias:
+        # if path == webpath.get_full_path():
+            # return item
+    # for child in item.get_childs():
+        # result = _get_current_item(child, path, language)
+        # if result: return result
+    # return None
 
 
 @register.simple_tag(takes_context=True)
@@ -61,8 +78,16 @@ def load_current_item_from_menu(context):
 
     request = context['request']
     language = getattr(request, 'LANGUAGE_CODE', '')
-    path = context['page'].webpath.get_full_path()
 
-    for item in context['items']:
-        result = _get_current_item(item, path, language)
-        if result: return result
+    item = NavigationBarItem.objects.filter(menu=context['items'][0].menu,
+                                            is_active=True,
+                                            webpath=context['page'].webpath,
+                                            webpath__alias__isnull=True,
+                                            webpath__is_active=True).first()
+    return item if item else None
+
+    # path = context['page'].webpath.get_full_path()
+
+    # for item in context['items']:
+        # result = _get_current_item(item, path, language)
+        # if result: return result
