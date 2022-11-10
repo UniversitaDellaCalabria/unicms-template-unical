ff19c3efe21ec6ed1ac357e1c5c1e47f20e3e6e7
Francesco Filicetti <francesco.filicetti@unical.it>
2022-11-10 09:36:54 +0100

    fix: css

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PKG_NAME = 'unicms_template_unical'
 
 setup(
     name=PKG_NAME,
-    version='1.1.1',
+    version='1.1.2',
 
     packages=[PKG_NAME],
     package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
