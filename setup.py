831bde63ac2c7ced94c5c94f75a6697620643bb6
Francesco Filicetti <francesco.filicetti@unical.it>
2022-11-24 23:39:26 +0100

    fix: Exbriner template

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PKG_NAME = 'unicms_template_unical'
 
 setup(
     name=PKG_NAME,
-    version='1.1.9',
+    version='1.1.10',
 
     packages=[PKG_NAME],
     package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
