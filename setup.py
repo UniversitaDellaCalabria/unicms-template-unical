92bc138acef8a0d70c705355fb0cb5abf15f5c6b
Francesco Filicetti <francesco.filicetti@unical.it>
2024-06-25 15:53:11 +0200

    v1.6.18

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PKG_NAME = 'unicms_template_unical'
 
 setup(
     name=PKG_NAME,
-    version='1.6.17',
+    version='1.6.18',
 
     packages=[PKG_NAME],
     package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
