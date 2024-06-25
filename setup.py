3d0319261cf3ebf80cc364d010ecac7d780e3b9a
Francesco Filicetti <francesco.filicetti@unical.it>
2024-06-25 18:40:20 +0200

    fix: v.1.6.22 + figure.image img height

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PKG_NAME = 'unicms_template_unical'
 
 setup(
     name=PKG_NAME,
-    version='1.6.21',
+    version='1.6.22',
 
     packages=[PKG_NAME],
     package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
