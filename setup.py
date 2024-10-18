a6b3c5483ce882940e402740d6d47ab4c22bae40
Francesco Filicetti <francesco.filicetti@unical.it>
2024-10-18 13:00:36 +0200

    feat: template ATLANTE

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PKG_NAME = 'unicms_template_unical'
 
 setup(
     name=PKG_NAME,
-    version='1.6.27',
+    version='1.6.28',
 
     packages=[PKG_NAME],
     package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
