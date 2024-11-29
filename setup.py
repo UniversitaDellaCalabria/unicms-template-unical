40243986e7bf3b0ab03dc3a5a62e1c721b1cff73
Francesco Filicetti <francesco.filicetti@unical.it>
2024-11-29 14:34:04 +0100

    chore: v1.8.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PKG_NAME = 'unicms_template_unical'
 
 setup(
     name=PKG_NAME,
-    version='1.8.4',
+    version='1.8.5',
 
     packages=[PKG_NAME],
     package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
