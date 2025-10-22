226b70e987870ba1d060b8560f1a8b501efae7dc
Francesco Filicetti <easyweb.wa@gmail.com>
2025-10-22 11:58:32 +0200

    chore: v1.8.16

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PKG_NAME = 'unicms_template_unical'
 
 setup(
     name=PKG_NAME,
-    version='1.8.15',
+    version='1.8.16',
 
     packages=[PKG_NAME],
     package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
