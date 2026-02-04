318f80248eebc506086d4f923d8a28ce63e8a7f5
Francesco Filicetti <easyweb.wa@gmail.com>
2026-02-04 10:04:01 +0100

    feat: SMOTION template

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ PKG_NAME = "unicms_template_unical"
 
 setup(
     name=PKG_NAME,
-    version="1.8.19",
+    version="1.8.20",
     packages=[PKG_NAME],
     package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
     package_data={
