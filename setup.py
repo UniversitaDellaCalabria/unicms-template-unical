094fb72b1cc0e1fd4c72990dd90a5c6eee86d88e
Francesco Filicetti <easyweb.wa@gmail.com>
2026-07-07 16:54:32 +0200

    fix: wrong name

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ PKG_NAME = "unicms_template_unical"
 
 setup(
     name=PKG_NAME,
-    version="1.9.1",
+    version="1.9.2",
     packages=[PKG_NAME],
     package_dir={PKG_NAME: f"{SRC_FOLDER}/{PKG_NAME}"},
     package_data={
